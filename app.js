bd6fe6c9308df54341162615dedd3793004d2600
Rohan Gupta <rohangupta788@gmail.com>
2021-04-02 01:05:07 +0530

    Changed h1 in signup

--- app.js
+++ app.js
@@ -61,7 +61,7 @@ app.post("/failure",function(req,res){
 });
 
 
-app.listen(process.env.PORT || 3000,function(){
+app.listen(process.env.PORT || 3000 ,function(){
   console.log("server is running.");
 });
 
